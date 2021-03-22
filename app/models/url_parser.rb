ba488dcccce694df05bca30b55f9182f2936807b
RSijelmass <R.Sijelmass@gmail.com>
2021-03-22 20:47:10 +0000

    Add is_valid_url? to UrlParser to ensure valid URLs
    To ensure we are working with URLs, we use Ruby's internal URI::regexp
    method to check whether we consider the URL to be valid. We currently do
    not check if the URL actually exists, only that its layout passes our
    criteria.
    
    We check is_valid_url? against email address, random string, and an
    instance with trailing whitespaces. We expect it to fail in all
    instances.

--- app/models/url_parser.rb
+++ app/models/url_parser.rb
@@ -1,5 +1,10 @@
 class UrlParser
-  def create_shorthand_for_url
+  def is_valid_url?(potential_url)
+    valid = potential_url.match(/\A#{URI::regexp}\z/)
+    !!valid
+  end
+
+  def create_shorthand
     SecureRandom.alphanumeric
   end
 end
--- spec/models/url_parser_spec.rb
+++ spec/models/url_parser_spec.rb
@@ -2,14 +2,34 @@ require 'rails_helper'
 
 RSpec.describe UrlParser do
   let(:url_parser) { UrlParser.new }
-  let(:valid_url) { "https://www.someExternalUrl.com/someEndpoint" }
-  let(:expected_shorthand) { "abcd1234" }
+  context 'create_shorthand_for_url' do
+    let(:expected_shorthand) { "abcd1234" }
+    before do
+      allow(SecureRandom).to receive(:alphanumeric).and_return(expected_shorthand)
+    end
 
-  before do
-    allow(SecureRandom).to receive(:alphanumeric).and_return(expected_shorthand)
+    it 'returns a shorthand version for a valid URL' do
+      expect(url_parser.create_shorthand).to eq expected_shorthand
+    end
   end
 
-  it 'returns a shorthand version for a valid URL' do
-    expect(url_parser.create_shorthand_for_url).to eq expected_shorthand
+  context 'is_valid_url?' do
+    let(:valid_url) { "https://www.someExternalUrl.com/someEndpoint" }
+    let(:invalid_url_email) { "someExternalEmail@gmail.com" }
+    let(:invalid_url_random_string) { "someExternalEmail@gmail.com" }
+    let(:invalid_url_trailing_spaces) { valid_url + "   " }
+
+    it 'will tell if we consider the url to be valid' do
+      expect(url_parser.is_valid_url? valid_url).to be true
+    end
+    it 'will tell if we consider the url to be invalid when an email is passed' do
+      expect(url_parser.is_valid_url? invalid_url_email).to be false
+    end
+    it 'will tell if we consider the url to be invalid when a random string is passed' do
+      expect(url_parser.is_valid_url? invalid_url_random_string).to be false
+    end
+    it 'will tell if we consider the url to be invalid when a valid URL with trailing spaces is passed' do
+      expect(url_parser.is_valid_url? invalid_url_trailing_spaces).to be false
+    end
   end
 end
