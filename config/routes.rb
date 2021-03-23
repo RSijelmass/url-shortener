1be83189cdadf54a0595f4ab78b43abc46042b42
RSijelmass <R.Sijelmass@gmail.com>
2021-03-23 19:33:55 +0000

    Initialise Sessions and Home controller
    initialise both the sessions and home controller to keep track if a user 
    is logged in or out. The home controller is the root endpoint a user 
    will be redirected to after login.
    
    Most instances are fully set up by rails. I have added the routes to 
    config/routes.rb to have more friendly endpoints in our routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  root "home#index"
+  
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :url_instances
   match '/short/:shorthand', to: 'url_instances#redirect_from_short', via: 'get'
 end
